f216c7e300c5bf510e4734657038310dc4f338c5
Marty Combs <github@martycombs.com>
2024-11-06 18:32:42 -0600

    Finish test script.

--- create-metadata/test.py
+++ create-metadata/test.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 from metadata import MetaData
+from mylog import MyLog
 
+l = MyLog(debug=True)
+log = l.log
+log.debug('Loading MetaData class')
 m = MetaData(debug=True)
 m.set_filename('create_metadata.sh')
 m.set_backup_date('2024-09-30 17:11:29 -0600')
@@ -10,11 +14,15 @@ m.add_file_stats()
 m.set_s3_url('s3://some-bucket/some/path')
 m.set_s3_url_metadata('s3://some-bucket/some/other/path')
 filecontents = m.format()
-print(filecontents)
-with open(m.metadata_filename, 'w') as f:
+div = '='*76
+log.debug('File contents will be:\n{}\n{}\n{}'.format(div, filecontents, div))
+log.debug('Metadata file name should be create_metadata.sh.meta')
+log.debug('Writing "{}"'.format(m.md_filename))
+with open(m.md_filename, 'w') as f:
     f.write(filecontents)
 f.close()
 
+log.debug('Reading "create_metadata.sh.meta"')
 m2 = MetaData(debug=True)
 m2.load('create_metadata.sh.meta')
 print(m2.format())
