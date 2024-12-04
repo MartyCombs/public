fd6b168738127fdaa77ad58b2894ff5085a73893
Marty Combs <github@martycombs.com>
2024-12-03 20:12:28 -0600

    Minor comment updates.

--- encrypt_files/bin/gen_new_key.py
+++ encrypt_files/bin/gen_new_key.py
@@ -6,7 +6,16 @@
 #=============================================================================#
 
 import cryptography
-
 from cryptography.fernet import Fernet
+
+# Generates a random, 32-bit master key sent to STDOUT.  This can be
+# pasted to the master key file for AES-GCM encryption.
+
 key = Fernet.generate_key()[:-1]
 print(key.decode())
+
+
+
+#=============================================================================#
+# END
+#=============================================================================#
